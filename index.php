f88b7ebac785ace6cadda2c7b594f3005b94f4bb
PERWIRA203 <140954608+PERWIRA203@users.noreply.github.com>
2025-10-01 17:23:06 +0700

    homepage

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Pacifico" />
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/index.css">
+    <title>Document</title>
+</head>
+<body>
+    <header class="header">
+        <div class="logo">
+            <img src="image/logo.jpg" alt="Logo" width="100" height="100">
+        </div>
+        <nav class="navbar">
+            <a href="#" class="active">Home</a>
+            <a href="about.php" >About Us</a>
+            <a href="facility.html">Facility</a>
+            <a href="menu.php">Menu</a>
+            <a href="review.php">Review</a>
+            <a href="contact.php">Contact</a>
+        </nav>
+        <div class="signup">
+            <a href="signup.php">Sign Up</a>
+        </div>
+    </header>
+    <main class="main">
+        <div class="background">
+            <div class="opening">
+                <p>Dari secangkir kopi, kami ingin menciptakan jutaan kenangan.</p>
+            </div>
+            <div class="button-reservation">
+                <a href="reservation.php">Reservation Now</a>
+            </div>
+        </div>
+        <div>
+            test
+        </div>
+    </main>
+</body>
+</html>
